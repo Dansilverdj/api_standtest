2da1732a659dc008a733518dc7ba4fb76e0dfd89
J Daniel Silvestre <danielsilvestredjWESSDgit config --global user.email danielsilvestredj@gmail.com>
2025-07-08 12:46:25 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://cnt-253497bb-69ef-41c5-a74e-45900ee3fc4d.containerhub.tripleten-services.com/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
